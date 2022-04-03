dac109e201d3c1788c8497aad801ad62cdbdce0a
Smit Patel <smitp520.sp@gmail.com>
2022-04-03 14:21:52 -0500

    name animation added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,54 @@
 import './index.scss'
 import logoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['m', 'i', 't']
+  const jobArray = [
+    's',
+    'o',
+    'f',
+    't',
+    'w',
+    'a',
+    'r',
+    'e',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+  ]
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I'm
-          <img src={logoTitle} alt="developer" />
-          mit
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
           <br />
-          fullstack developer
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={logoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={18}
+          />
         </h1>
         <h2>Full Stack Developer / UI Designer / Freelancer</h2>
         <Link to="/contact" className="flat-button">
