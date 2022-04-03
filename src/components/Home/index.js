37efef1e8a9e052508e0e38b32dfeaf71996c1b3
Smit Patel <smitp520.sp@gmail.com>
2022-04-03 01:21:13 -0500

    animations added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,25 @@
 import './index.scss'
+import logoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-  return <>Home</>
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm
+          <img src={logoTitle} alt="developer" />
+          mit
+          <br />
+          fullstack developer
+        </h1>
+        <h2>Full Stack Developer / UI Designer / Freelancer</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default Home
